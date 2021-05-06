94de045234c04cbe01801634ac839b4b087307f6
daineAMD <daine.mcniven@amd.com>
2021-05-06 15:31:44 -0600

    Clang format and readme fixes.

--- Extensions/gemm_ex_f16_r/README.md
+++ Extensions/gemm_ex_f16_r/README.md
@@ -1,11 +1,11 @@
-# rocBLAS-Examples gemm_ex_f32_r
+# rocBLAS-Examples gemm_ex_f16_r
 Example showing moving matrix rocblas_f32 data to the GPU device and calling the rocblas gemm_ex (general matrix matrix product) function. Results are fetched from GPU and compared against a CPU implementation and displayed.
 
 ## Documentation
 Run the example without any command line arguments to use default values.
 Running with --help will show the options:
 
-    Usage: ./gemm_ex_f32_r
+    Usage: ./gemm_ex_f16_r
       --K <value>              Matrix/vector dimension
       --M <value>              Matrix/vector dimension
       --N <value>              Matrix/vector dimension
--- Extensions/gemm_ex_f16_r/gemm_ex_f16_r.cpp
+++ Extensions/gemm_ex_f16_r/gemm_ex_f16_r.cpp
@@ -19,8 +19,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-#include <cmath>
 #include "helpers.hpp"
+#include <cmath>
 #include <hip/hip_runtime.h>
 #include <math.h>
 #include <rocblas.h>
@@ -132,18 +132,12 @@ int main(int argc, char** argv)
         }
 
         // copy data from CPU to device
-        CHECK_HIP_ERROR(
-            hipMemcpy(dA, hA.data(), sizeof(_Float16) * sizeA, hipMemcpyHostToDevice));
-        CHECK_HIP_ERROR(
-            hipMemcpy(dB, hB.data(), sizeof(_Float16) * sizeB, hipMemcpyHostToDevice));
-        CHECK_HIP_ERROR(hipMemcpy(dC,
-                                  static_cast<void*>(hC.data()),
-                                  sizeof(_Float16) * sizeC,
-                                  hipMemcpyHostToDevice));
-        CHECK_HIP_ERROR(hipMemcpy(dD,
-                                  static_cast<void*>(hD.data()),
-                                  sizeof(_Float16) * sizeD,
-                                  hipMemcpyHostToDevice));
+        CHECK_HIP_ERROR(hipMemcpy(dA, hA.data(), sizeof(_Float16) * sizeA, hipMemcpyHostToDevice));
+        CHECK_HIP_ERROR(hipMemcpy(dB, hB.data(), sizeof(_Float16) * sizeB, hipMemcpyHostToDevice));
+        CHECK_HIP_ERROR(hipMemcpy(
+            dC, static_cast<void*>(hC.data()), sizeof(_Float16) * sizeC, hipMemcpyHostToDevice));
+        CHECK_HIP_ERROR(hipMemcpy(
+            dD, static_cast<void*>(hD.data()), sizeof(_Float16) * sizeD, hipMemcpyHostToDevice));
 
         // enable passing alpha parameter from pointer to host memory
         rstatus = rocblas_set_pointer_mode(handle, rocblas_pointer_mode_host);
@@ -180,8 +174,7 @@ int main(int argc, char** argv)
         CHECK_ROCBLAS_STATUS(rstatus);
 
         // fetch device memory results, automatically blocked until results ready
-        CHECK_HIP_ERROR(
-            hipMemcpy(hD.data(), dD, sizeof(_Float16) * sizeD, hipMemcpyDeviceToHost));
+        CHECK_HIP_ERROR(hipMemcpy(hD.data(), dD, sizeof(_Float16) * sizeD, hipMemcpyDeviceToHost));
 
     } // release device memory via helpers::DeviceVector destructors
 
