d65994f534c83471aceff9dfdbe15ac34f3993e4
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2023-09-21 15:57:01 -0600

    Update CHANGELOG.md, fixes for CI & later version, improve readability  (#88)
    * updates for ROCm 5.7
    * trmv example tidied
    * avoid deprecated paths for tools and fortran mod 
    * hip folder in rocm deprecated

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -1,13 +1,13 @@
 // This file is for internal AMD use.
 // If you are interested in running your own Jenkins, please raise a github issue for assistance.
 
-def runCompileCommand(platform, project, jobName)
+def runCompileCommand(platform, project, jobName, boolean sameOrg=false)
 {
     project.paths.construct_build_prefix()
 
     def sudo = platform.jenkinsLabel.contains('sles') ? '/usr/bin/sudo --preserve-env ' : ''
     String centos = platform.jenkinsLabel.contains('centos') ? 'source scl_source enable devtoolset-7' : ''
-    def getDependencies = auxiliary.getLibrary('rocBLAS-internal',platform.jenkinsLabel,'develop')
+    def getDependencies = auxiliary.getLibrary('rocBLAS-internal',platform.jenkinsLabel, null, sameOrg)
     def command = """#!/usr/bin/env bash
                 set -x
                 cd ${project.paths.project_build_prefix}
@@ -16,18 +16,18 @@ def runCompileCommand(platform, project, jobName)
                 ${centos}
                 ${sudo} make
             """
-    
+
     platform.runCommand(this, command)
 }
 
 def runTestCommand (platform, project)
 {
     def sudo = auxiliary.sudo(platform.jenkinsLabel)
-        
+
     def command = """#!/usr/bin/env bash
                 set -x
                 cd ${project.paths.project_build_prefix}
-                ${sudo} make run 2>&1 | tee test_log 
+                ${sudo} make run 2>&1 | tee test_log
                 grep -ni error test_log
                 grep -ni warning test_log
                 grep -ni fail test_log
